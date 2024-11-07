70612e0194c9dca233eae56e840df7c45b9f8337
henrav <henrik@ravnborg.se>
2024-11-07 14:25:00 +0100

    la till i gitignore

--- projekt/Server/index.js
+++ projekt/Server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { exec } = require('child_process');
+const mysql = require("mysql2");
 const app = express();
 const PORT = process.env.PORT || 5001;
+require('dotenv').config();
+
 
 
 //skapa databas
@@ -12,10 +15,36 @@ exec('node CreateDatabase.js', (error, stdout, stderr) => {
     }
     console.log('Database Created Successfully');
 });
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    multipleStatements: true
+});
 
 
 app.get('/', (req, res) => {
     res.send('Server is running with sample data.');
+});
+
+app.get('/getModuler', async (req, res) => {
+    const querystring = 'CALL mydb.EpokDBGetModul(?);';
+    const kursKod = req.query.kursKod;
+    if (!kursKod) {
+        res.status(400).send('Missing required query parameter: kursKod');
+        return;
+    }
+
+    db.query(querystring, [kursKod], (err, results) => {
+        if (err) {
+            console.error('Failed to fetch moduler:', err);
+            res.status(500).send('Failed to fetch moduler');
+            return;
+        }
+        res.json(results[0]);
+    });
+
 });
 
 //testing
